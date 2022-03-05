24fa2d8be5e2e518121ef96fe783269d4df00399
tahabenk <tahabenkirane@gmail.com>
2022-03-05 12:49:17 +0100

    toilet_model 2

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,3 +1,6 @@
 class Toilet < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :longtitude, :latitude, :description, :address, :toilets_option:, :shower_option, :baby_changing_table_option, presence: true
 end
