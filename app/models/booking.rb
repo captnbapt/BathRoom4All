d36db8d49ce59da1f07e7ae771a4236d71eb51ee
tahabenk <tahabenkirane@gmail.com>
2022-03-05 13:20:02 +0100

    booking model done

---
+++ app/models/booking.rb
@@ -0,0 +1,16 @@
+class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :toilet
+  has_many :review
+
+  validate :end_date_is_after_start_date?
+  validates :from_date, :to_date, presence: true
+
+  private
+
+  def end_date_is_after_start_date?
+    if from_date.present? && to_date.present? && to_date < from_date
+      self.errors[:base] << "Check-out cannot be before Check-in"
+    end
+  end
+end
