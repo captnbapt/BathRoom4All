d230ccd3aee5588abaa804b2b316fb43ac440552
tahabenk <tahabenkirane@gmail.com>
2022-03-10 17:35:13 +0100

    Added routes to bathrooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  resources :bathrooms do
+    collection do
+      get :my_bathrooms
+    end
+    resources :bookings, only: %i[create show]
+  end
   devise_for :users
+  resources :bookings, only: %i[index]
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
